731180619311cf2b24dd2c18be22ebee274a98fc
Markus Wamser <wamser@tum.de>
2016-01-15 15:26:04 +0100

    added Author info and Licence, renamed main file

--- R/vcd2r.R
+++ R/vcd2r.R
@@ -1,7 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
+# Main package file to set up otions, version strings etc.
 #
 # You can learn more about package authoring with RStudio at:
 #
@@ -13,6 +10,3 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
