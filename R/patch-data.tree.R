ec12528022218025b027decedd88151ad7816c0e
Markus Wamser <wamser@tum.de>
2016-02-17 17:05:09 +0100

    Improved Find() to search arbitrary fields.

--- R/patch-data.tree.R
+++ R/patch-data.tree.R
@@ -4,17 +4,18 @@
 
 # data.tree does not provide a find function
 
-Find <- function(node, name, traversal = c("pre-order", "post-order", "in-order", "level",
+Find <- function(node, value, field="name", traversal = c("pre-order", "post-order", "in-order", "level",
                                      "ancestor"), all = F){
 
   trav <- data.tree::Traverse(node, traversal, pruneFun = NULL, filterFun = NULL)
-  ret <- trav[sapply(trav,function(x) x$name == name)]
+  ret <- trav[sapply(trav,function(x) any(x[[field]] == value))]
 
   if (length(ret) == 0) return(NULL)
   if (!all) return(ret[[1]])
   return(ret)
 
 }
+
 
 #trav <- Traverse(node, traversal, pruneFun = NULL, filterFun = NULL)
 FindFirstFast <- function(trav, name){
