a979ee87afc8af74943fade4a58e7004b219610a
Markus Wamser <github-dev@mail2013.wamser.eu>
2016-12-16 21:22:15 +0100

    fx test

--- tests/testthat/test-vcd-special.R
+++ tests/testthat/test-vcd-special.R
@@ -12,13 +12,38 @@ test_that("sanity checks in parseVCDForKeys work",{
 })
 
 test_that("detection of malformed entries in VCD works",{
-  longwarning<-"$upscope missing $end
-Malformed VCD file: $upscope outside scope at offset 0364 bytes in input file.
-Invalid statement at offset 0599 bytes in scope definition: invalidtoken
-Invalid keyword at offset 0642 bytes in scope definition: $invalidkeyword
-Invalid keyword at offset 0659 bytes in scope definition: $end
-Invalid keyword at offset 0754 bytes in scope definition: $dumpvars
-Invalid statement at offset 0964 bytes in scope definition: invalidtoken
-multiple top modules, only lastest is kept. At offset 0723 bytes in input file."
-  expect_warning(vcd<-VCDFile("wikipedia-mod.vcd"),longwarning)
+  # see http://stackoverflow.com/questions/35712544/how-to-test-for-multiple-warnings-in-an-unknown-order-using-testthat/35730070#35730070 #nolint
+
+  gotWarnings<-character(0)
+  withCallingHandlers({
+    vcd<-VCDFile("wikipedia-mod.vcd")
+  },
+    warning=function(e) {
+    # Push warning onto vector in parent frame.
+    gotWarnings <<- c(gotWarnings, conditionMessage(e))
+    invokeRestart("muffleWarning")
+  })
+
+  longwarning<-c("$upscope missing $end",
+                 "Malformed VCD file: $upscope outside scope at offset 0364 bytes in input file.",
+                 "Invalid statement at offset 0599 bytes in scope definition: invalidtoken",
+                 "Invalid keyword at offset 0642 bytes in scope definition: $invalidkeyword",
+                 "Invalid keyword at offset 0659 bytes in scope definition: $end",
+                 "Invalid keyword at offset 0754 bytes in scope definition: $dumpvars",
+                 "Invalid statement at offset 0964 bytes in scope definition: invalidtoken",
+                 "multiple top modules, only lastest is kept. At offset 0723 bytes in input file.")
+  # Ensure no unexpected warnings,
+  expect_equal(length(gotWarnings), 8)
+
+  # Test that each warning I want is there
+  # all.equal qould be shorter, but this gives better reporting
+  expect_equal(longwarning[1], gotWarnings[1])
+  expect_equal(longwarning[2], gotWarnings[2])
+  expect_equal(longwarning[3], gotWarnings[3])
+  expect_equal(longwarning[4], gotWarnings[4])
+  expect_equal(longwarning[5], gotWarnings[5])
+  expect_equal(longwarning[6], gotWarnings[6])
+  expect_equal(longwarning[7], gotWarnings[7])
+  expect_equal(longwarning[8], gotWarnings[8])
+
 })
